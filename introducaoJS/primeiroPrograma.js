2cb1b7efd5f15ff719d75c24849c95b2a5be18f8
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-10-19 09:41:46 -0300

    Add files

---
+++ introducaoJS/primeiroPrograma.js
@@ -0,0 +1,9 @@
+console.log("Meu primeiro Programa: Trabalhando com variáveis");
+
+const idade = 26;
+
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+console.log(idade*2);
