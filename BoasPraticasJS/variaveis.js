c586508bbfe4867f2bc78d6885e23ca119db4145
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-10-19 10:25:16 -0300

    pasta BoasPraticas criada

---
+++ BoasPraticasJS/variaveis.js
@@ -0,0 +1,21 @@
+console.log("variáveis");
+
+const idade = 29;
+const nome = "Carlos";
+
+//operações aritméticas
+console.log("aritméticas");
+console.log(2 + 2);
+console.log(2 + 2 * 10);
+console.log((2+2)*10);
+
+//conversão de tipos
+console.log("conversão de tipos");
+console.log("ano " + 2020);
+console.log(parseInt(2)+parseInt(2));
+console.log("10" / "2");
+console.log("Ricardo" / "2");//Not a Number
+console.log(6.5);
+console.log(6,5);
+
+//
