28da98981104cfe1031496dfff9454a5f5e17ee2
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-10-20 09:44:02 -0300

    BoasPraticas finalizada

---
+++ BoasPraticasJS/atribuicaoVariaveis.js
@@ -0,0 +1,10 @@
+console.log("Trabalhando com atribuições");
+
+const nome = "Carlos";
+const sobrenome = "Tsukamoto";
+
+console.log(nome, sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
