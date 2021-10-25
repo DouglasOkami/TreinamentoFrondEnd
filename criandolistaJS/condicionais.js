54b909c9d1db6fb63aad4a961d3e33b5922b004a
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-10-24 23:18:33 -0300

    atuazações do projet criandoLista

--- criandolistaJS/condicionais.js
+++ criandolistaJS/condicionais.js
@@ -1,18 +1,29 @@
 console.log("Trabalhando com condicionais");
 
 const listaDeDestinos = new Array(
-    `Salvador`, `Rio de Janeiro` ,`São Paulo`
+    `Salvador`, `Rio de Janeiro`, `São Paulo`
 );
 
-const idade = 15;
+const idade = 18;
+const estaAcompanhada = false;
+const temPassagem = true;
+
 console.log("Possiveis destinos :");
 console.log(listaDeDestinos);
 
-if(idade >= 18){
-    console.log("Comprador maior de idade");
-    listaDeDestinos.splice(1,1);
-}else{
+if (idade >= 18 || (estaAcompanhada == true)) {
+    console.log("Boa Viagem");
+    listaDeDestinos.splice(2, 1);
+}
+else {
     console.log("Comprador menor de idade não posso vender");
+}
+
+console.log("Embarque: \n");
+if (idade >= 18 && temPassagem ) {
+    console.log("Boa viagem")
+}else{
+    console.log("Você não pode embarcar")
 }
 
 console.log(listaDeDestinos);
